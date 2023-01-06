042da264cdcced9f422da008741069c67f0b8198
Tim Mussack <tim.mussack@gmail.com>
2023-01-06 16:51:57 -0600

    App.js updated with KeyboardAvoidingView wrapper element

--- App.js
+++ App.js
@@ -1,47 +1,63 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 
-//All elements and native hooks have to imported from "react-native", no <div>, <img>, <p>, <li> etc.
-import { StyleSheet, Text, View, Button, Image, useWindowDimensions } from "react-native";
+//All elements and native hooks must be imported from "react-native", no <div>, <img>, etc.
+import { StyleSheet, Text, View, Button, Image, useWindowDimensions, TextInput, KeyboardAvoidingView } from "react-native";
 
 
 const App = () => {
   //useState, useRef, useEffect all work like React
   const [showImage, setShowImage] = useState(false);
   const [changeColor, setChangeColor] = useState(false);
-  //React Native hook to help track screen orientation a useColorScheme() hook also exists
+  const [darkMode, setDarkMode] = useState(false);
+  const [text, setText] = useState("");
+
+  //This is a React Native hook to help track screen orientation a useColorScheme() hook also exists
   const {height, width} = useWindowDimensions();
 
-  //Decides flexDirection by asking if screen is landscape ? render elements left to right otherwise top to bottom
+  //Decides flexDirection by asking if screen is landscape, renders elements left to right in landscape otherwise top to bottom
   let flexType = width > height ? "row" : "column";
 
-  //changeColor === true ? make text red otherwise black
-  let textColor = changeColor ? "#D30000" : "#212121";
+  //Title text color variable
+  let textColor = changeColor ? "#D30000" : "#000000";
+
+  //Status bar and background color variables, both change with darkMode state
+  let statusBar = darkMode ? "light" : "dark"
+  let mode = darkMode ? "#363636" : "#FFFFFF"
 
   return (
-    //Parent View element container with 4 child elements, flexDirection is defined inline so that the flexType variable is in scope
-    <View style={[ styles.container, {flexDirection: flexType} ]}>
+    //Parent element container with 5 child elements, flexDirection is defined inline so that the flexType variable is in scope
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={[ styles.container, {flexDirection: flexType, backgroundColor: mode} ]}
+    >
 
-      {/* Child View element 1 */}
+      {/* Allows access to phones status bar */}
+      <StatusBar style={statusBar} />
+
+      {/* Child View element 1, text element with a press event that changes text color*/}
       <View style={styles.view1}>
         <Text
-          style={{ alignSelf: "center", fontWeight: "bold", fontSize: 20, color: textColor, padding: 5 }}>
+          style={[ styles.title, {color: textColor}]}
+          onPress={() => {
+            setChangeColor(!changeColor);
+          }}>
           React Native & Expo Go
         </Text>
       </View>
 
-      {/* Child View element 2 */}
+      {/* Child View element 2, button element with press event that alerts and changes colors of background and status bar*/}
       <View style={styles.view2}>
         <Button
           onPress={() => {
-            setChangeColor(!changeColor);
-            alert("Text color changed!");
+            setDarkMode(!darkMode)
+            alert("Theme changed!");
           }}
-          title="Press Me"
+          title="Dark Mode"
         />
       </View>
 
-      {/* Child View element 3 */}
+      {/* Child View element 3, button element with press event that toggles a state*/}
       <View style={styles.view3}>
         <Button
           onPress={() => {
@@ -52,62 +68,86 @@ const App = () => {
         />
       </View>
 
-      {/* Child View element 4 */}
+      {/* Child View element 4, image element that is conditionally rendered*/}
       <View style={styles.view4}>
         { showImage &&
           <Image
           source={ require('./assets/dog.png') }
-          style={styles.dog}
+          style={[ styles.dog, {backgroundColor: mode} ]}
         /> }
       </View>
 
-    </View>
+      {/* Child TextInput element, wrapper KeyboardAvoidingView ensures the keyboard does not block the users view */}
+      <TextInput
+        style={styles.input}
+        onChangeText={setText}
+        value={text}
+        placeholder="Input text"
+      />
 
-    // Create an error
-    //<Text></Text>
+    </KeyboardAvoidingView>
+
+    //<Text>Force an error</Text>
   );
 };
 
+//StyleSheet is an abstraction similar to CSS stylesheet syntax
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 15,
-    borderWidth: 5,
+    borderWidth: 4,
     borderColor: "#3BB143",
     borderRadius: 7,
   },
   view1: {
     flex: 1,
-    borderWidth: 2,
-    borderColor: "#212121",
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#000000",
     borderRadius: 5,
     justifyContent: "center",
+    margin: 1,
   },
   view2: {
     flex: 1,
-    borderWidth: 2,
-    borderColor: "#212121",
+    borderWidth: 1,
+    borderColor: "#000000",
     borderRadius: 5,
     justifyContent: "center",
+    margin: 1,
   },
   view3: {
     flex: 1,
     borderWidth: 2,
-    borderColor: "#212121",
+    borderColor: "#000000",
     borderRadius: 5,
     justifyContent: "center",
+    margin: 1,
   },
   view4: {
     flex: 3,
-    borderWidth: 2,
-    borderColor: "#212121",
+    borderWidth: 3,
+    borderColor: "#000000",
     borderRadius: 5,
     justifyContent: "center",
+    margin: 1,
   },
   dog: {
-    backgroundColor: "#FFFFFF",
     width: 150,
     position: "relative",
+  },
+  title: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    fontSize: 20,
+    padding: 5,
+  },
+  input: {
+    height: 40,
+    margin: 1,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
   },
 });
 
